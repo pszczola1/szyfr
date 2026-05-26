5b7aebeff4d4396535d5b89dbce88db8ec7a46ea
pszczola <181292276+pszczola1@users.noreply.github.com>
2026-05-26 23:38:18 +0200

    Added browse feature and qol

--- windows.py
+++ windows.py
@@ -1,12 +1,12 @@
 from PySide6.QtCore import Qt, QObject, Signal, QThread
 from PySide6.QtWidgets import (QWidget, QLabel, QVBoxLayout, QButtonGroup,
-    QRadioButton, QLineEdit, QPushButton, QProgressBar)
+    QRadioButton, QLineEdit, QPushButton, QProgressBar, QMessageBox, QFileDialog)
 from encryption import encrypt, decrypt
-
 
 class Worker(QObject):
     progress = Signal(int)
     finished = Signal()
+    error = Signal(str)
 
     def __init__(self, file, password, mode=None):
         super().__init__()
@@ -18,7 +18,10 @@ class Worker(QObject):
         if self.mode is not None:
             encrypt(self.file, self.password, self.mode, progress_signal=self.progress.emit)
         else:
-            decrypt(self.file, self.password, progress_signal=self.progress.emit)
+            try:
+                decrypt(self.file, self.password, progress_signal=self.progress.emit)
+            except ValueError as e:
+                self.error.emit(str(e))
         self.finished.emit()
 
 class MainWindow(QWidget):
@@ -26,12 +29,17 @@ class MainWindow(QWidget):
         super().__init__()
 
         self.resize(400, 400)
+        self.setWindowTitle("File encryptor")
 
         self.label = QLabel("Drop a file here")
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
+        self.file_dialog_button = QPushButton("Browse")
+        self.file_dialog_button.clicked.connect(self._show_file_dialog)
+
         layout = QVBoxLayout()
         layout.addWidget(self.label)
+        layout.addWidget(self.file_dialog_button)
 
         self.setLayout(layout)
 
@@ -49,8 +57,16 @@ class MainWindow(QWidget):
         if not paths: return
 
         path = paths[0].toLocalFile()
-        print(path)
 
+        self._create_enc_window(path)
+
+    def _show_file_dialog(self):
+        path, _ = QFileDialog.getOpenFileUrl(self, "Select file", "", "")
+        if path.isEmpty(): return
+        print(path.path()[1:])
+        self._create_enc_window(path.path()[1:])
+
+    def _create_enc_window(self, path):
         self.enc_window = EncWindow(path)
         self.enc_window.show()
 
@@ -61,13 +77,16 @@ class EncWindow(QWidget):
         self.file = dropped_file_path
 
         self.resize(400, 200)
+        self.setWindowTitle(" ")
 
         self.label = QLabel("Encrypt or decrypt a file")
         self.label.setAlignment(Qt.AlignmentFlag.AlignTop)
 
         self.work_mode_rg = QButtonGroup()
         self.encrypt_button = QRadioButton("Encrypt")
-        self.decrypt_button = QRadioButton("decrypt")
+        self.decrypt_button = QRadioButton("Decrypt")
+        self.encrypt_button.clicked.connect(self._enable_mode_buttons)
+        self.decrypt_button.clicked.connect(self._disable_mode_buttons)
         self.work_mode_rg.addButton(self.encrypt_button)
         self.work_mode_rg.addButton(self.decrypt_button)
 
@@ -110,6 +129,9 @@ class EncWindow(QWidget):
         else: return
 
         password = self.password_input.text()
+        if len(password) < 8:
+            QMessageBox.critical(self, "Incorrect password", "Password has to be at least 8 characters long")
+            return
 
         self._add_progress_bar()
 
@@ -137,11 +159,25 @@ class EncWindow(QWidget):
 
         self.thread.started.connect(self.worker.run)
         self.worker.progress.connect(self.progress_bar.setValue)
+        self.worker.error.connect(self.show_error)
 
         self.worker.finished.connect(self.thread.quit)
         self.worker.finished.connect(self.worker.deleteLater)
         self.thread.finished.connect(self.thread.deleteLater)
         self.thread.finished.connect(self.close)
+
+    def _enable_mode_buttons(self):
+        self.ecb_button.setDisabled(False)
+        self.cbc_button.setDisabled(False)
+        self.ctr_button.setDisabled(False)
+
+    def _disable_mode_buttons(self):
+        self.ecb_button.setDisabled(True)
+        self.cbc_button.setDisabled(True)
+        self.ctr_button.setDisabled(True)
+
+    def show_error(self, error_message):
+        QMessageBox.critical(self, "Error", error_message)
 
 
 
